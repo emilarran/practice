204ed8ab57a5e5b5eb18b2e1f2313ccdf6def8f1
Emil Arran Baleva <arranbaleva@gmail.com>
2018-02-26 00:53:17 +0800

    Added user registration, profile, edit user data

--- project/accounts/views.py
+++ project/accounts/views.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from accounts.forms import RegistrationForm
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserChangeForm
 
 # Create your views here.
 def home(request):
@@ -21,3 +22,19 @@ def register(request):
 		form = RegistrationForm()
 		args = {'form': form}
 		return render(request, 'accounts/reg_form.html', args)
+
+def view_profile(request):
+	args = {'user': request.user}
+	return render(request, 'accounts/profile.html',args)
+
+def edit_profile(request):
+	if request.method == 'POST':
+		form = UserChangeForm(request.POST, instance=request.user)
+
+		if form.is_valid():
+			form.save()
+			return redirect('/account/profile')
+	else:
+		form = UserChangeForm(instance=request.user)
+		args = {'form':form}
+		return render(request, 'accounts/edit_profile.html',args)
